9a5aa46c484ff960c21d11988a9a3e8c18c8cda4
sNAtttu <nsanteri@hotmail.com>
2015-09-11 21:20:22 +0300

    first commit

---
+++ HappyOfficeService/Controllers/PointController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.WindowsAzure.Mobile.Service;
+using HappyOfficeService.DataObjects;
+using HappyOfficeService.Models;
+
+namespace HappyOfficeService.Controllers
+{
+    public class PointController : TableController<Point>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            HappyOfficeContext context = new HappyOfficeContext();
+            DomainManager = new EntityDomainManager<Point>(context, Request, Services);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<Point> GetAllTodoItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<Point> GetTodoItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<Point> PatchTodoItem(string id, Delta<Point> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostTodoItem(Point item)
+        {
+            Point current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTodoItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
---
+++ HappyOfficeService/DataObjects/Point.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace HappyOfficeService.DataObjects
+{
+    public class Point : EntityData
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Altitude { get; set; }
+        public double Lux { get; set; }
+        public double Desibel { get; set; }
+        public string Username { get; set; }
+    }
+}
